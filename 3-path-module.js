2909a9f4724b898431ea662f2708571c250a37b9
chauhan-09 <nsc7574@gmail.com>
2024-11-25 22:18:15 +0530

    made changes

--- 3-path-module.js
+++ 3-path-module.js
@@ -1,3 +1,17 @@
 const path = require('path');
 
-console.log(path.sep);
+//separator is a property
+console.log(path.sep); 
+
+//join method combines the path string/segements to give us the file path
+const filepath = path.join('content','subfoler','test.txt');
+console.log(filepath);
+
+//basename extracts the file name from given path
+const data = path.basename(filepath);
+console.log(data);
+
+//absolute path is the complete path
+//resolve gives the absolute path takes path string/segments as inputs and combines them
+const absolute = path.resolve(__dirname,'content','subfolder','test.txt');
+console.log(absolute);
