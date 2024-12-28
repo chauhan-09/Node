98171d0abf45a2f8d25da1a57c93d2f893d55920
chauhan-09 <nsc7574@gmail.com>
2024-12-28 13:51:34 +0530

    made changes

--- 23-Git_Theory.js
+++ 23-Git_Theory.js
@@ -2,5 +2,7 @@
 
 Git is a version control system . Saves a particular version of your source code basically saves a snapshot of your code
 
+why we should not save node_modules folder to github?
 
+1. Huge size 
 */
